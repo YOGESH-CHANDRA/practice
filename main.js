2b6e95fb047cf68eefb7ed69aa83225238df6467
TechnicalSupervisor <yogeshchandra52@gmail.com>
2023-07-31 10:58:20 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function raj(){
+    console.log("My name is yogee");
+}
+
+// raj();
+
+document.write("Hello");
+let x;
+x="<h2>Hi maanni <h2>";
+// console.log(nam);
+
+// let y=document.querySelector("#h1");
+let btn=document.getElementById("btn");
+btn.addEventListener("click", ()=>{
+    nam="my name is hero";
+    console.log(nam); 
+    let abc;
+    abc=nam;
+    document.body.style.background="green";
+    document.getElementsByTagName("h1").innerHTML=x;
+
+})
